5e8d24a4dc31f64b4d559ed6407ab6b08cf5604b
Jaswanth Singh Kumar Lankadasu <jaswanthlsvyj@gmail.com>
2024-07-07 20:43:53 +0530

    first push in DSL

---
+++ 1 Python/calculator.py
@@ -0,0 +1,14 @@
+# defining the constant var in the file
+CONSTANT = 'Jaswanth'
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
